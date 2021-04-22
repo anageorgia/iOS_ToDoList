0bc8b5aa536bb1f31372cd052e5f6a0f2bcad47f
Ana Geórgia <anageorgiagp@gmail.com>
2021-04-21 21:02:37 -0300

    added elements of table view cell

---
+++ ToDoList/Controllers/TasksTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TasksTableViewController.swift
+//  ToDoList
+//
+//  Created by Ana Geórgia Gama on 21/04/21.
+//
+
+import UIKit
+
+class TasksTableViewController: UITableViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
